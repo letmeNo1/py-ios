79825f6bc0c06460c81feac0b18f8767cb94c05d
hank.huang <fkzzzz@qq.com>
2025-02-24 09:16:26 +0800

    INIT

---
+++ py_ios/execute_cmd.py
@@ -0,0 +1,26 @@
+﻿import os
+import subprocess
+import argparse
+
+def run_exe(*cmd_args):
+    # Get the full path of the executable
+    exe_path = os.path.join(os.getcwd(), "lib", "windows","go-ios.exe")
+    
+    # Construct the command with the executable path and the variable arguments
+    command = f'"{exe_path}" --userspace ' + ' '.join(cmd_args)
+    
+    # Execute the command
+    subprocess.run(command, shell=True)
+
+def main():
+    # Create the argument parser
+    parser = argparse.ArgumentParser(description="Run go-ios.exe with specified arguments.")
+    
+    # Add arguments to the parser
+    parser.add_argument('cmd_args', nargs=argparse.REMAINDER, help="Arguments to pass to go-ios.exe")
+    
+    # Parse the arguments
+    args = parser.parse_args()
+    
+    # Call run_exe with the parsed arguments
+    run_exe(*args.cmd_args)
---
+++ setup.py
@@ -0,0 +1,31 @@
+﻿from setuptools import setup, find_packages
+
+setup(
+    name='py_ios',  # Replace with your project's name
+    version='0.1.0',  # Initial version
+    author='Your Name',  # Replace with your name
+    author_email='your.email@example.com',  # Replace with your email
+    description='A brief description of your project',
+    long_description=open('README.md').read(),  # Ensure you have a README.md file
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/yourproject',  # Replace with your project's URL
+    packages=find_packages(),  # Automatically find packages in your project
+    install_requires=[
+        # List your project's dependencies here, e.g.,
+        # 'requests>=2.25.1',
+    ],
+     package_data={
+        'py_ios': ['lib/windows/*']
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # Choose your license
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Specify the Python version requirement
+    entry_points={
+        'console_scripts': [
+            'idb=py_ios.execute_cmd:main',  # Replace with your command and module
+        ],
+    },
+)
